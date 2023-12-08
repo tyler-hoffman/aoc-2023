bfaa652a871230e0a992dccfb904a8edaa40bc73
Tyler Hoffman <tyler.hoffman@greatexpectations.io>
2023-12-08 08:21:25 -0500

    More stuff around factors

--- aoc_2023/tools/factors.py
+++ aoc_2023/tools/factors.py
@@ -1,4 +1,4 @@
-from typing import Mapping
+from typing import Collection, Mapping
 
 from aoc_2023.tools.optimus import Optimus
 
@@ -18,4 +18,25 @@ def get_prime_factors(x: int) -> Mapping[int, int]:
         if count:
             output[prime] = count
 
+    return output
+
+
+def smallest_product_of_all_factors(values: Collection[int]) -> int:
+    """IDK the right name for this."""
+    factor_sets = [get_prime_factors(v) for v in values]
+
+    relevant_primes = set[int]()
+    for factors in factor_sets:
+        relevant_primes.update(set(factors.keys()))
+
+    output_factors = dict[int, int]()
+    for prime in relevant_primes:
+        for factor in factor_sets:
+            output_factors[prime] = max(
+                factor.get(prime, 0), output_factors.get(prime, 0)
+            )
+
+    output = 1
+    for k, v in output_factors.items():
+        output *= k**v
     return output
--- tests/test_tools/test_factors.py
+++ tests/test_tools/test_factors.py
@@ -1,6 +1,6 @@
 import pytest
 
-from aoc_2023.tools.factors import get_prime_factors
+from aoc_2023.tools.factors import get_prime_factors, smallest_product_of_all_factors
 
 
 @pytest.mark.parametrize(
@@ -15,3 +15,15 @@ from aoc_2023.tools.factors import get_prime_factors
 )
 def test_get_prime_factors(input: int, expected: int):
     assert get_prime_factors(input) == expected
+
+
+@pytest.mark.parametrize(
+    "input, expected",
+    [
+        ([2], 2),
+        ([2, 4], 4),
+        ([6, 9], 18),
+    ],
+)
+def test_smallest_product_of_all_factors(input: list[int], expected: int):
+    assert smallest_product_of_all_factors(input) == expected
