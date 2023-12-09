b8f906f3c71cdba39e55121830e55562a87a5931
Tyler Hoffman <tyler.hoffman@greatexpectations.io>
2023-12-09 08:42:10 -0500

    Solve 09.a

--- aoc_2023/day_09/a.py
+++ aoc_2023/day_09/a.py
@@ -4,11 +4,24 @@ from aoc_2023.day_09.parser import Parser
 
 @dataclass
 class Day09PartASolver:
-    data: list[list[int]]
+    lines: list[list[int]]
 
     @property
     def solution(self) -> int:
-        return -1
+        vals = [self.get_new_last_num(line) for line in self.lines]
+        return sum(vals)
+
+    @staticmethod
+    def get_new_last_num(vals: list[int]) -> int:
+        if all([val == 0 for val in vals]):
+            return 0
+        diff = Day09PartASolver.get_diff(vals)
+        last_diff = Day09PartASolver.get_new_last_num(diff)
+        return last_diff + vals[-1]
+
+    @staticmethod
+    def get_diff(vals: list[int]) -> list[int]:
+        return [vals[i] - vals[i - 1] for i in range(1, len(vals))]
 
 
 def solve(input: str) -> int:
--- tests/test_day_09/test_a.py
+++ tests/test_day_09/test_a.py
@@ -1,13 +1,10 @@
-import pytest
-
 from aoc_2023.day_09.a import get_solution, solve
-from aoc_2023.day_09.from_prompt import SAMPLE_DATA, SAMPLE_SOLUTION
+from aoc_2023.day_09.from_prompt import SAMPLE_DATA, SAMPLE_SOLUTION_A
 
 
 def test_solve():
-    assert solve(SAMPLE_DATA) == SAMPLE_SOLUTION
+    assert solve(SAMPLE_DATA) == SAMPLE_SOLUTION_A
 
 
-@pytest.mark.skip
 def test_my_solution():
-    assert get_solution() == "NOT THIS"
+    assert get_solution() == 1834108701
