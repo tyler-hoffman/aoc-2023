3147bbb12aedbc5a7afef96a69335c45581d1946
Tyler Hoffman <tyler.hoffman@greatexpectations.io>
2023-12-12 06:37:55 -0500

    Solve 12.a

--- aoc_2023/day_12/a.py
+++ aoc_2023/day_12/a.py
@@ -1,15 +1,57 @@
 from dataclasses import dataclass
+from typing import Iterator
 from aoc_2023.day_12.common import Record
 from aoc_2023.day_12.parser import Parser
 
 
 @dataclass
 class Day12PartASolver:
-    data: list[Record]
+    records: list[Record]
 
     @property
     def solution(self) -> int:
-        return -1
+        output = 0
+        for record in self.records:
+            output += len(list(self.get_variations(record.chars[:], record)))
+        return output
+
+    @staticmethod
+    def is_valid_sequence(seq: list[str], record: Record) -> bool:
+        congruencies = list[int]()
+        count = 0
+        prev = "."
+        for ch in seq:
+            if ch == "." and prev == "#":
+                if count:
+                    congruencies.append(count)
+                count = 0
+            if ch == "#":
+                count += 1
+            prev = ch
+        if count:
+            congruencies.append(count)
+        return congruencies == record.congruencies
+
+    def get_variations(
+        self, chars: list[str], record: Record, index: int = 0
+    ) -> Iterator[list[str]]:
+        if index == len(chars):
+            if self.is_valid_sequence(chars, record):
+                yield chars[:]
+        else:
+            match chars[index]:
+                case ".":
+                    yield from self.get_variations(chars, record, index + 1)
+                case "#":
+                    yield from self.get_variations(chars, record, index + 1)
+                case "?":
+                    chars[index] = "."
+                    yield from self.get_variations(chars, record, index + 1)
+                    chars[index] = "#"
+                    yield from self.get_variations(chars, record, index + 1)
+                    chars[index] = "?"
+                case _:
+                    assert False
 
 
 def solve(input: str) -> int:
