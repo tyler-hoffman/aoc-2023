97e3f337d6c8da70d15b0930196441da663ac572
Tyler Hoffman <tyler.hoffman@greatexpectations.io>
2023-12-14 06:06:50 -0500

    Solve 14.a

--- aoc_2023/day_14/a.py
+++ aoc_2023/day_14/a.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from functools import cached_property
 from aoc_2023.day_14.parser import Parser
 
 
@@ -8,7 +9,31 @@ class Day14PartASolver:
 
     @property
     def solution(self) -> int:
-        return -1
+        return sum(self.solve_col(x) for x in range(self.width))
+
+    def solve_col(self, x: int) -> int:
+        dest = 0
+        output = 0
+        for y in range(self.height):
+            match self.panel[y][x]:
+                case ".":
+                    ...
+                case "#":
+                    dest = y + 1
+                case "O":
+                    output += self.height - dest
+                    dest += 1
+                case _:
+                    assert False
+        return output
+
+    @cached_property
+    def width(self) -> int:
+        return len(self.panel[0])
+
+    @cached_property
+    def height(self) -> int:
+        return len(self.panel)
 
 
 def solve(input: str) -> int:
