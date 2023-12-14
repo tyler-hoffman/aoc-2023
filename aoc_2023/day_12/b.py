6280100ee58fb0de6153aac586017fa8a879ddb9
Tyler Hoffman <tyler.hoffman@greatexpectations.io>
2023-12-13 20:15:00 -0500

    Remove old stuff

--- aoc_2023/day_12/b.py
+++ aoc_2023/day_12/b.py
@@ -1,12 +1,11 @@
 from dataclasses import dataclass
 from functools import cached_property
-from typing import Iterator, Optional
-from aoc_2023.day_12.common import Record
+from aoc_2023.day_12.common import Day12Solver, Record
 from aoc_2023.day_12.parser import Parser
 
 
 @dataclass(frozen=True)
-class Day12PartBSolver:
+class Day12PartBSolver(Day12Solver):
     original_records: list[Record]
 
     @cached_property
@@ -22,61 +21,6 @@ class Day12PartBSolver:
             chars.pop()
             output.append(Record(chars, congruencies))
         return output
-
-    @property
-    def solution(self) -> int:
-        output = 0
-        for record in self.records:
-            output += len(list(self.get_variations(record.chars[:], record)))
-        return output
-
-    @staticmethod
-    def is_valid_sequence(seq: list[str], record: Record) -> bool:
-        return Day12PartBSolver.get_congruencies(seq) == record.congruencies
-
-    @staticmethod
-    def get_congruencies(seq: list[str], length: Optional[int] = None) -> list[int]:
-        end = length if length is not None else len(seq)
-        congruencies = list[int]()
-        count = 0
-        prev = "."
-        for i, ch in enumerate(seq):
-            if i >= end:
-                break
-            if ch == "." and prev == "#":
-                if count:
-                    congruencies.append(count)
-                count = 0
-            if ch == "#":
-                count += 1
-            prev = ch
-        if count:
-            congruencies.append(count)
-        return congruencies
-
-    def get_variations(
-        self, chars: list[str], record: Record, index: int = 0
-    ) -> Iterator[list[str]]:
-        congruencies = self.get_congruencies(chars[:index])
-        if congruencies[:-1] != record.congruencies[: len(congruencies)][:-1]:
-            return
-        if index == len(chars):
-            if self.is_valid_sequence(chars, record):
-                yield chars[:]
-        else:
-            match chars[index]:
-                case ".":
-                    yield from self.get_variations(chars, record, index + 1)
-                case "#":
-                    yield from self.get_variations(chars, record, index + 1)
-                case "?":
-                    chars[index] = "."
-                    yield from self.get_variations(chars, record, index + 1)
-                    chars[index] = "#"
-                    yield from self.get_variations(chars, record, index + 1)
-                    chars[index] = "?"
-                case _:
-                    assert False
 
 
 def solve(input: str) -> int:
