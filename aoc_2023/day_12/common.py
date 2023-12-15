19883ae0bb01bb0b951bb8ed6d95c54bc05a98c0
Tyler Hoffman <tyler.hoffman@greatexpectations.io>
2023-12-14 20:42:32 -0500

    Solve 12.b

--- aoc_2023/day_12/common.py
+++ aoc_2023/day_12/common.py
@@ -1,7 +1,7 @@
+from dataclasses import dataclass
 import re
 from abc import ABC, abstractmethod
-from dataclasses import dataclass, field
-from functools import cached_property
+from functools import cache
 
 
 @dataclass(frozen=True)
@@ -22,125 +22,116 @@ class Day12Solver(ABC):
 
     @property
     def solution(self) -> int:
-        return sum(RecordSolver(r).variations for r in self.records)
+        output = 0
+        for record in self.records:
+            output += solve_it(
+                chars=tuple(record.chars[:]),
+                congruencies=tuple(record.congruencies[:]),
+            )
+        return output
 
 
-@dataclass(frozen=True)
-class RecordSolver:
-    record: Record = field(hash=False)
-
-    @cached_property
-    def variations(self) -> int:
-        return self.solve_it(
-            chars=self.record.chars[:],
-            congruencies=self.record.congruencies[:],
-        )
-
-    @staticmethod
-    def solve_it(
-        chars: list[str],
-        congruencies: list[int],
-    ) -> int:
-        if len(congruencies) == 0:
-            output = 1 if all(ch != "#" for ch in chars) else 0
-            return output
-        elif len(congruencies) == 1:
-            return RecordSolver.congruency_matches("".join(chars), congruencies[0])
+@cache
+def solve_it(
+    chars: tuple[str, ...],
+    congruencies: tuple[int, ...],
+) -> int:
+    if len(congruencies) == 0:
+        output = 1 if all(ch != "#" for ch in chars) else 0
+        return output
+    elif len(congruencies) == 1:
+        return congruency_matches("".join(chars), congruencies[0])
+    else:
+        if len(chars) == 0:
+            return 0
+        mid = len(chars) // 2
+        mid_char = chars[mid]
+        if mid_char == ".":
+            return split_as_period(chars, congruencies)
+        elif mid_char == "#":
+            return split_as_hash(chars, congruencies)
+        elif mid_char == "?":
+            return sum(
+                [
+                    split_as_period(chars, congruencies),
+                    split_as_hash(chars, congruencies),
+                ]
+            )
         else:
-            if len(chars) == 0:
+            assert False
+
+
+def congruency_matches(chars: str, congruency: int) -> int:
+    if congruency == 0:
+        return 1 if "#" not in chars else 0
+    groups = [g for g in period_pattern.split(chars) if g]
+    with_hashes = [g for g in groups if "#" in g]
+    match len(with_hashes):
+        case 0:
+            output = 0
+            for group in groups:
+                if len(group) >= congruency:
+                    output += len(group) + 1 - congruency
+            return output
+        case 1:
+            string = with_hashes[0]
+            hash_start = string.index("#")
+            hash_end = len(string) - string[::-1].index("#")
+            length = hash_end - hash_start
+            if length > congruency:
                 return 0
-            mid = len(chars) // 2
-            mid_char = chars[mid]
-            if mid_char == ".":
-                return RecordSolver.split_as_period(chars, congruencies)
-            elif mid_char == "#":
-                return RecordSolver.split_as_hash(chars, congruencies)
-            elif mid_char == "?":
-                return sum(
-                    [
-                        RecordSolver.split_as_period(chars, congruencies),
-                        RecordSolver.split_as_hash(chars, congruencies),
-                    ]
-                )
             else:
-                assert False
-
-    @staticmethod
-    def congruency_matches(chars: str, congruency: int) -> int:
-        if congruency == 0:
-            return 1 if "#" not in chars else 0
-        groups = [g for g in period_pattern.split(chars) if g]
-        with_hashes = [g for g in groups if "#" in g]
-        match len(with_hashes):
-            case 0:
                 output = 0
-                for group in groups:
-                    if len(group) >= congruency:
-                        output += len(group) + 1 - congruency
+                for start in range(1 + len(string) - congruency):
+                    end = start + congruency
+                    if start > hash_start:
+                        ...
+                    elif end < hash_end:
+                        ...
+                    else:
+                        output += 1
                 return output
-            case 1:
-                string = with_hashes[0]
-                hash_start = string.index("#")
-                hash_end = len(string) - string[::-1].index("#")
-                length = hash_end - hash_start
-                if length > congruency:
-                    return 0
-                else:
-                    output = 0
-                    for start in range(1 + len(string) - congruency):
-                        end = start + congruency
-                        if start > hash_start:
-                            ...
-                        elif end < hash_end:
-                            ...
-                        else:
-                            output += 1
-                    return output
 
-            case _:
-                return 0
+        case _:
+            return 0
 
-    @staticmethod
-    def split_as_period(chars: list[str], congruencies: list[int]) -> int:
-        mid = len(chars) // 2
-        assert chars[mid] != "#"
-        left_chars = chars[:mid]
-        right_chars = chars[mid + 1 :]
-        output = 0
-        for i in range(len(congruencies) + 1):
-            left = RecordSolver.solve_it(left_chars, congruencies[:i])
-            right = RecordSolver.solve_it(right_chars, congruencies[i:])
-            output += left * right
-        return output
 
-    @staticmethod
-    def split_as_hash(chars: list[str], congruencies: list[int]) -> int:
-        output = 0
-        mid = len(chars) // 2
-        assert chars[mid] != "."
-        congruency_count = len(congruencies)
-        output = 0
-        for c in range(congruency_count):
-            congruency = congruencies[c]
-            for to_the_left in range(congruency):
-                left = mid - to_the_left
-                right = left + congruency
-                if left < 0:
-                    continue
-                if right > len(chars):
-                    continue
-                if "." in chars[left:right]:
-                    continue
-                if left > 0 and chars[left - 1] == "#":
-                    continue
-                if right < len(chars) and chars[right] == "#":
-                    continue
+def split_as_period(chars: tuple[str, ...], congruencies: tuple[int, ...]) -> int:
+    mid = len(chars) // 2
+    assert chars[mid] != "#"
+    left_chars = chars[:mid]
+    right_chars = chars[mid + 1 :]
+    output = 0
+    for i in range(len(congruencies) + 1):
+        left = solve_it(left_chars, congruencies[:i])
+        right = solve_it(right_chars, congruencies[i:])
+        output += left * right
+    return output
 
-                left_count = RecordSolver.solve_it(
-                    chars[: max(0, left - 1)], congruencies[:c]
-                )
-                right_count = RecordSolver.solve_it(
-                    chars[right + 1 :], congruencies[c + 1 :]
-                )
-                output += left_count * right_count
-        return output
+
+def split_as_hash(chars: tuple[str, ...], congruencies: tuple[int, ...]) -> int:
+    output = 0
+    mid = len(chars) // 2
+    assert chars[mid] != "."
+    congruency_count = len(congruencies)
+    output = 0
+    for c in range(congruency_count):
+        congruency = congruencies[c]
+        for to_the_left in range(congruency):
+            left = mid - to_the_left
+            right = left + congruency
+            if left < 0:
+                continue
+            if right > len(chars):
+                continue
+            if "." in chars[left:right]:
+                continue
+            if left > 0 and chars[left - 1] == "#":
+                continue
+            if right < len(chars) and chars[right] == "#":
+                continue
+
+            left_count = solve_it(chars[: max(0, left - 1)], congruencies[:c])
+            right_count = solve_it(chars[right + 1 :], congruencies[c + 1 :])
+            output += left_count * right_count
+    return output
--- tests/test_day_12/test_b.py
+++ tests/test_day_12/test_b.py
@@ -1,5 +1,3 @@
-import pytest
-
 from aoc_2023.day_12.b import get_solution, solve
 from aoc_2023.day_12.from_prompt import SAMPLE_DATA, SAMPLE_SOLUTION_B
 
@@ -8,6 +6,5 @@ def test_solve():
     assert solve(SAMPLE_DATA) == SAMPLE_SOLUTION_B
 
 
-@pytest.mark.skip
 def test_my_solution():
-    assert get_solution() == "NOT THIS"
+    assert get_solution() == 17391848518844
