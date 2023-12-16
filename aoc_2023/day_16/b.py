e489361d55f603e95af0193f9d33a5ca5f1ca79a
Tyler Hoffman <tyler.hoffman@greatexpectations.io>
2023-12-16 09:05:42 -0500

    Solve 16.b

--- aoc_2023/day_16/b.py
+++ aoc_2023/day_16/b.py
@@ -1,14 +1,29 @@
 from dataclasses import dataclass
+from functools import cached_property
+from aoc_2023.day_16.common import DOWN, LEFT, RIGHT, UP, Beam, Grid
 from aoc_2023.day_16.parser import Parser
+from aoc_2023.tools.point import Point
 
 
 @dataclass
 class Day16PartBSolver:
-    input: str
+    chars: list[str]
+
+    @cached_property
+    def grid(self) -> Grid:
+        return Grid(self.chars)
 
     @property
     def solution(self) -> int:
-        return -1
+        counts = set[int]()
+        g = self.grid
+        for y in range(g.height):
+            counts.add(g.get_energized(Beam(Point(0, y), RIGHT)))
+            counts.add(g.get_energized(Beam(Point(g.width - 1, y), LEFT)))
+        for x in range(g.width):
+            counts.add(g.get_energized(Beam(Point(x, 0), DOWN)))
+            counts.add(g.get_energized(Beam(Point(x, g.height - 1), UP)))
+        return max(counts)
 
 
 def solve(input: str) -> int:
--- tests/test_day_16/test_b.py
+++ tests/test_day_16/test_b.py
@@ -1,18 +1,10 @@
-import pytest
-
 from aoc_2023.day_16.b import get_solution, solve
+from aoc_2023.day_16.from_prompt import SAMPLE_DATA, SAMPLE_SOLUTION_B
 
 
-@pytest.mark.parametrize(
-    "input, expected",
-    [
-        ("INPUT", -1),
-    ],
-)
-def test_solve(input: str, expected: int):
-    assert solve(input) == expected
+def test_solve():
+    assert solve(SAMPLE_DATA) == SAMPLE_SOLUTION_B
 
 
-@pytest.mark.skip
 def test_my_solution():
-    assert get_solution() == "NOT THIS"
+    assert get_solution() == 8754
