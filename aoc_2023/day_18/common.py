c89d9c74971ef5df4b9fc5d07a5a03ddb6ea7bd9
Tyler Hoffman <tyler.hoffman@greatexpectations.io>
2023-12-18 07:05:29 -0500

    Solve 18.a

--- aoc_2023/day_18/common.py
+++ aoc_2023/day_18/common.py
@@ -1,8 +1,23 @@
 from dataclasses import dataclass
 
+from aoc_2023.tools.point import Point
+
 
 @dataclass
 class Instruction:
     direction: str
     length: int
     color: str
+
+
+UP = Point(0, -1)
+DOWN = Point(0, 1)
+LEFT = Point(-1, 0)
+RIGHT = Point(1, 0)
+
+INSTRUCTION_MAP = {
+    "U": UP,
+    "D": DOWN,
+    "L": LEFT,
+    "R": RIGHT,
+}
--- tests/test_day_18/test_a.py
+++ tests/test_day_18/test_a.py
@@ -1,5 +1,3 @@
-import pytest
-
 from aoc_2023.day_18.a import get_solution, solve
 from aoc_2023.day_18.from_prompt import SAMPLE_DATA, SAMPLE_SOLUTION_A
 
@@ -8,6 +6,5 @@ def test_solve():
     assert solve(SAMPLE_DATA) == SAMPLE_SOLUTION_A
 
 
-@pytest.mark.skip
 def test_my_solution():
-    assert get_solution() == "NOT THIS"
+    assert get_solution() == 40714
