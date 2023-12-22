7e5bf680a8e7e28cffa78325d8008a99b3855260
Tyler Hoffman <tyler.hoffman@greatexpectations.io>
2023-12-22 10:55:15 -0500

    Solve 22.a

--- aoc_2023/day_22/common.py
+++ aoc_2023/day_22/common.py
@@ -1,19 +1,42 @@
 from __future__ import annotations
 from dataclasses import dataclass
+from typing import Optional
 
 
-@dataclass
+@dataclass(frozen=True)
 class Brick:
     start: Point
     end: Point
+    name: Optional[str]
 
     def __post_init__(self) -> None:
         assert self.end.x >= self.start.x
         assert self.end.y >= self.start.y
         assert self.end.z >= self.start.z
 
+    @property
+    def height(self) -> int:
+        return self.end.z + 1 - self.start.z
 
-@dataclass
+    def drop(self, z: int) -> Brick:
+        return Brick(
+            Point(self.start.x, self.start.y, z),
+            Point(self.end.x, self.end.y, z + self.height - 1),
+            self.name,
+        )
+
+    def overlaps(self, other: Brick) -> bool:
+        return all(
+            [
+                self.end.x >= other.start.x,
+                self.start.x <= other.end.x,
+                self.end.y >= other.start.y,
+                self.start.y <= other.end.y,
+            ]
+        )
+
+
+@dataclass(frozen=True)
 class Point:
     x: int
     y: int
